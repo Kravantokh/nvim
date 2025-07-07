4bdf545de2ea0113425d6fa61c4907e4b4f705ba
kravantokh <>
2025-07-07 20:56:15 +0300

    Fixed some errors.

--- README.md
+++ README.md
@@ -11,11 +11,15 @@ NOTE: This configuration will change over time as I establish my preferences and
 * Session tracking[^2] (Obsession)
 * Compile/run[^3]
 * Syntax Highlighting (Treesitter)
-* Code folding based on Treesitter
+* Code folding based (Treesitter)
+* Real-time error-checking for C/C++ (clangd)
 
 [^1]: `F1` can be used to toggle the file tree on and off.
 [^2]: Can be toggled on and off with `F2`. It is off by default. To return to a session use `nvim -S Session.vim` in the folder where the session tracking was done.
 [^3]: Compilation with `F5`, run with `F6` provided by a `compile.sh` (or `compile.bat` on Windows) and a `run.sh` (`run.bat`) script files in the parent directory of the project.
+
+# Prerequisites
+* clangd (for error checking and possibly autocomplete)
 
 # Installation
 The repository has `nvim` as the name so that it can be directly cloned into `.config` on linux or `AppData\Local` on Windows and it will automatically have the correct name to be recognized by neovim.
--- init.lua
+++ init.lua
@@ -1,5 +1,4 @@
--- Show line numbering
-vim.api.nvim_command('set number relativenumber')
+-- Show line numbering vim.api.nvim_command('set number relativenumber')
 -- Highligt horizontal and vertical position of the cursor
 vim.api.nvim_command('set cursorline')
 vim.api.nvim_command('set cursorcolumn')
@@ -47,6 +46,12 @@ require('packer').startup(function()
 	
 	-- session manager
 	use('tpope/vim-obsession')
+
+	-- Autocomplete backend
+	use 'neovim/nvim-lspconfig'
+	
+	-- Autocomplete menu
+	use 'neoclide/coc.nvim'
 end)
 
  -- theme setup
@@ -76,6 +81,42 @@ require('nvim-treesitter.configs').setup {
 	}
 }
 
+-- Language server setup
+require'lspconfig'.clangd.setup{}
+
+-- Mappings.
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+local opts = { noremap=true, silent=true }
+vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, opts)
+
+-- Mappings.
+-- See `:help vim.lsp.*` for documentation on any of the below functions
+local bufopts = { noremap=true, silent=true, buffer=bufnr }
+	vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+	vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+	vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+	vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+	vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+	vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+	vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+	vim.keymap.set('n', '<space>wl', function()
+	print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+end, bufopts)
+	vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
+	vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+	vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
+	vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+	vim.keymap.set('n', '<space>f', vim.lsp.buf.formatting, bufopts)
+
+local lsp_flags = {
+-- This is the default in Nvim 0.7+
+	debounce_text_changes = 150,
+}
+
+
  -- Mapping F1 to file manager open and close
 vim.api.nvim_set_keymap('n', '<F1>', ':NvimTreeToggle<ENTER>', { noremap = true, silent = true })
 
@@ -87,7 +128,8 @@ if vim.fn.has('unix') == 1 then
 	vim.api.nvim_set_keymap('n', '<F5>', ':!./compile.sh<ENTER>', { noremap = true, silent = true })
 
 elseif vim.fn.has('win32') == 1 then
-	vim.api.nvim_set_keymap('n', '<F5>', ':! .\\compile.bat & exit<CR>', { noremap = true, silent = true })	
+	-- vim.api.nvim_set_keymap('n', '<F5>', ':new | r ! .\\compile.bat :setlocal buftype=nofile :setlocal bufhidden=hide :setlocal noswapfile<CR>', { noremap = true, silent = true })	
+	vim.api.nvim_set_keymap('n', '<F5>', ':! .\\compile.bat<CR>', { noremap = true, silent = true })	
 end
 
 -- Mapping F6 to run the run script in the opened folder
@@ -95,5 +137,7 @@ if vim.fn.has('unix') == 1 then
 	vim.api.nvim_set_keymap('n', '<F6>', ':!./run.sh<ENTER>', { noremap = true, silent = true })
 
 elseif vim.fn.has('win32') == 1 then
-	vim.api.nvim_set_keymap('n', '<F6>', ':! .\\run.bat & exit<CR>', { noremap = true, silent = true })	
+	vim.api.nvim_set_keymap('n', '<F6>', ':!.\\run.bat & exit<CR>', { noremap = true, silent = true })	
+
 end
+
